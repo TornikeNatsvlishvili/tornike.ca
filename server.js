933d1bffa29b4df55946b65ed38c1debc7c27a21
tnatsvli <tnatsvli@mail.uoguelph.ca>
2015-07-14 22:59:06 -0700

    Going back to basics

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 var app     = express();
 
 var PORT = parseInt(process.env.PORT) || 8080;
@@ -14,4 +14,11 @@ app.get('/ping', function(req, res){
 })
 
 app.listen(PORT);
-console.log('Started server on port: ' + PORT);
+console.log('Started server on port: ' + PORT);*/
+
+var http = require('http');
+var port = process.env.port || 1337;
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
