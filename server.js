02444f8513deb19d5baf0c1add410ad8da34e598
tnatsvli <tnatsvli@mail.uoguelph.ca>
2015-07-14 23:48:51 -0700

    trying something basic with express

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ var http = require('http');
 var express = require('express');
 
 var app = express();
-
 var PORT = parseInt(process.env.PORT) || 8080;
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-    res.sendfile('/public/pages/index.html', { root: __dirname });
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+    //res.sendfile('/public/pages/index.html', { root: __dirname });
 });
-
 app.get('/ping', function(req, res){
     res.json("pong");
 })
