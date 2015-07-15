fbef840ddfb05f12488dae20016ecc8148929477
tnatsvli <tnatsvli@mail.uoguelph.ca>
2015-07-14 23:24:44 -0700

    Adding log lines

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var http = require('http');
 var express = require('express');
 
-var app     = express();
+var app = express();
 
 var PORT = parseInt(process.env.PORT) || 8080;
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-    res.sendfile('./public/pages/index.html');
+    res.sendfile('/public/pages/index.html', { root: __dirname });
 });
 
 app.get('/ping', function(req, res){
@@ -16,3 +16,4 @@ app.get('/ping', function(req, res){
 })
 
 http.createServer(app).listen(PORT);
+console.log(stringify( process.env ));
