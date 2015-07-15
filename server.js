b0b0ec169b34fe0cb2940014a025278c010dfb1f
tnatsvli <tnatsvli@mail.uoguelph.ca>
2015-07-15 01:04:06 -0700

    create service with express

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ var express = require('express');
 
 var app = express();
 
+app.get('/', function(req, res){
+  res.json('Hello World\n');
+})
+
 var port = process.env.PORT || 8080;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+http.createServer(app).listen(port);
 /*
 var PORT = parseInt(process.env.PORT) || 8080;
 
-app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
