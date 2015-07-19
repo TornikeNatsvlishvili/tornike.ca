b3b838ed74dbb50e80c58fb209ee6dcb00fcc7a2
tnatsvli <tnatsvli@mail.uoguelph.ca>
2015-07-18 19:18:14 -0700

    changed styles added flag library

--- public/src/js/controllers/MainController.js
+++ public/src/js/controllers/MainController.js
@@ -2,4 +2,17 @@ angular.module('MainController', ['ngBreadcrumbs'])
 .controller('mainController', ['$scope', '$route', 'breadcrumbs', '$location', function($scope, $route, breadcrumbs, $location){
 	$scope.$route = $route;
 	$scope.breadcrumbs = breadcrumbs;
+	$scope.hi="hi";
+	$scope.getAge = function(){
+		var birth = new Date(1993,9,28);
+		var now = new Date();
+		
+		var timeDiff = Math.abs(now.getTime() - birth.getTime());
+		var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+		
+		var yearsOld = Math.floor(diffDays / 365);
+		var remainder = (diffDays % 365) / 365;
+		
+		return (yearsOld + remainder).toFixed(3).toString();
+	};
 }]);
