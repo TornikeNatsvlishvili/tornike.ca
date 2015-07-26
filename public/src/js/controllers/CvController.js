b494206f6f0ec243a97c06f75f3e994d497080cc
tnatsvli <tnatsvli@mail.uoguelph.ca>
2015-07-26 01:37:53 -0700

    adding markdown/cv, adding education, adding projects

--- public/src/js/controllers/CvController.js
+++ public/src/js/controllers/CvController.js
@@ -2,4 +2,14 @@ angular.module('CvController', [])
 
 .controller('cvController', ['$scope', function($scope){
 	$scope.message = '---c';
-}]);
+}])
+
+.directive('markdown', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var htmlText = markdown.toHTML(element.text());
+            element.html(htmlText);
+        }
+	};
+});
