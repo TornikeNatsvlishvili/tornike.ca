a1e03cc9c1c124410824fa817206a0a7fc158f5e
tnatsvli <tnatsvli@mail.uoguelph.ca>
2016-02-28 12:13:34 -0500

    Small changes

--- server.js
+++ server.js
@@ -12,18 +12,9 @@ app.get('/ping', function(req, res){
     res.json("pong");
 })
 
-var port = process.env.PORT || 8080;
-http.createServer(app).listen(port);
-/*
-
-
-app.get('/', function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-    //res.sendfile('/public/pages/index.html', { root: __dirname });
-});
-app.get('/ping', function(req, res){
-    res.json("pong");
+app.get('/notes', function(req, res){
+    res.json("hi")
 })
 
-http.createServer(app).listen(PORT);*/
+var port = process.env.PORT || 8080;
+http.createServer(app).listen(port);
