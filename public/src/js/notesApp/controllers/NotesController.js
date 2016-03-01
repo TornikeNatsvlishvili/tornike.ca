3c69caca5c03f87d439deab9375e82adcc64e32a
tnatsvli <tnatsvli@mail.uoguelph.ca>
2016-02-29 23:51:47 -0500

    finishing notes

--- public/src/js/notesApp/controllers/NotesController.js
+++ public/src/js/notesApp/controllers/NotesController.js
@@ -1,11 +1,36 @@
 angular.module('NotesController', [])
 
 .controller('notesController', ['$scope', '$http', function($scope, $http) {
-    $http.get('/api/notes/')
-        .success(function(data){
-            $scope.entries = data;
-        });
+    $scope.UpdateNotes = function(){
+       $http.get('/api/notes/')
+            .success(function(data){
+                $scope.entries = data;
+            });
+    };
+        
+    $scope.DeleteNote = function(index){
+        var noteId = $scope.entries[index].id;
+        $http.delete('/api/notes/' + noteId)
+            .success(function(data){
+                $scope.entries.splice(index, 1);
+            });
+    };
     
+    $scope.AddNote = function(){
+        var note = {'note': $scope.noteText,
+                    'created_date': new Date()};
+        $http.post('/api/notes/', note)
+            .success(function(data){
+                $scope.UpdateNotes();        
+            });
+    };
+    
+    $scope.ShowDialog = function(note){
+        $("<div>" + note + "</div>").dialog({
+            resizable: true,
+        });
+    };   
 
+    $scope.UpdateNotes();
 }]);
 
